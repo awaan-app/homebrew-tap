a734250a5bafc1c85f64c8790fb44ea654f63c39
Restu Muzakir <hi@restuhaqza.dev>
2026-06-12 17:43:41 +0700

    awaan 0.5.0

--- Casks/awaan.rb
+++ Casks/awaan.rb
@@ -1,11 +1,12 @@
 cask "awaan" do
-  version "0.3.0"
-  sha256 "b3b2c8a8498989bb3942a6f886cf79a4a2da91670c303acd6a41b55b966ef1d3"
+  version "0.5.0"
+  sha256 arm:   "e49114946fccac3cf50923db5560ba43a235fe736245d9d2addadd5db2f9fc0b",
+         intel: "e49114946fccac3cf50923db5560ba43a235fe736245d9d2addadd5db2f9fc0b"
 
-  url "https://7r9tr1bhb9tyyxrv.public.blob.vercel-storage.com/Awaan_0.3.0_aarch64.dmg"
+  url "https://7r9tr1bhb9tyyxrv.public.blob.vercel-storage.com/Awaan_0.5.0_universal.dmg"
   name "Awaan"
   desc "Privacy-first Islamic prayer times app for your desktop"
-  homepage "https://app.awaan.cloud"
+  homepage "https://awaan.cloud"
 
   livecheck do
     url "https://app.awaan.cloud/version.json"
@@ -23,8 +24,8 @@ cask "awaan" do
   end
 
   zap trash: [
-    "~/Library/Application Support/com.digitalsanctuary.awaan",
-    "~/Library/Preferences/com.digitalsanctuary.awaan.plist",
-    "~/Library/Saved Application State/com.digitalsanctuary.awaan.savedState",
+    "~/Library/Application Support/cloud.awaan.app",
+    "~/Library/Preferences/cloud.awaan.app.plist",
+    "~/Library/Saved Application State/cloud.awaan.app.savedState",
   ]
 end
